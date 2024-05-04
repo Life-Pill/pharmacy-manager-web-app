f3db47f7651dc99e71900d45a12e3cc9a6634460
Tharindu Jayawardhana <tharinduimalka915@gmail.com>
2024-05-04 10:16:46 +0530

    added the lottie for loading animation in branch

--- src/features/branch-management-dashboard/services/BranchManagementService.tsx
+++ src/features/branch-management-dashboard/services/BranchManagementService.tsx
@@ -19,6 +19,8 @@ const useBranchManagementService = () => {
     } catch (error) {
       console.log(error);
       toast.error('error');
+    } finally {
+      setLoadingAllBranchSales(false);
     }
   };
 
