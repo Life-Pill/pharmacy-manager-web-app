5ac4042c3101d8346dab63d60aa8790cb8595579
Tharindu Jayawardhana <tharinduimalka915@gmail.com>
2024-06-26 03:55:29 +0530

    config

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,15 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   server: {
-    port: 5173,
     proxy: {
-      ':8079/lifepill/v1': {
-        target: 'http://35.174.112.238:8079/lifepill/v1',
+      '/lifepill/v1': {
+        target: 'http://35.174.112.238:8079',
         changeOrigin: true,
+        secure: false,
         rewrite: (path) => path.replace(/^\/lifepill\/v1/, ''),
       },
     },
   },
-  plugins: [react()],
 });
