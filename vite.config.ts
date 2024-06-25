7da8542515d0f412e6981016bf7c192b815d0a23
Tharindu Jayawardhana <tharinduimalka915@gmail.com>
2024-06-26 03:39:45 +0530

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,10 @@ export default defineConfig({
   server: {
     port: 5173,
     proxy: {
-      '/api': {
+      '/lifepill/v1': {
         target: 'http://35.174.112.238:8079/lifepill/v1',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/lifepill\/v1/, ''),
       },
     },
   },
