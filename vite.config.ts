5fd7dd16c7a315e1c1e548628b74ec98a834088d
Tharindu Jayawardhana <tharinduimalka915@gmail.com>
2024-06-26 07:55:05 +0530

    corsssss

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,14 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   port: 5173, // Choose a different port for your Vite frontend development server
-  //   proxy: {
-  //     '/lifepill/v1': {
-  //       target: 'http://35.174.112.238', // Backend server running on port 8079
-  //       changeOrigin: true,
-  //       rewrite: (path) => path.replace(/^\/lifepill\/v1/, '/lifepill/v1'),
-  //     },
-  //   },
-  // },
+  server: {
+    port: 5173, // Choose a different port for your Vite frontend development server
+    proxy: {
+      '/lifepill/v1': {
+        target: 'http://35.174.112.238', // Backend server running on port 8079
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/lifepill\/v1/, '/lifepill/v1'),
+      },
+    },
+  },
 });
